7edf2b952cf445170680a358a386d6c1cea79b90
Huy Le <huylh@bap.jp>
2020-12-22 15:03:15 +0700

    Update PlayerView

--- TestVideo/PlayerView.swift
+++ TestVideo/PlayerView.swift
@@ -15,16 +15,11 @@ class PlayerView: UIView {
     return stickerContainers.subviews as? [UIImageView] ?? []
   }
 
-  var stickersCIImage: CIImage? {
-    guard !stickers.isEmpty, let cgImage = stickerContainers.asImage().cgImage else { return nil }
-    return CIImage(cgImage: cgImage)
-  }
-
   override static var layerClass: AnyClass {
     AVPlayerLayer.self
   }
   
-  var playerLayer: AVPlayerLayer {
+  private var playerLayer: AVPlayerLayer {
     layer as! AVPlayerLayer
   }
   
@@ -33,18 +28,25 @@ class PlayerView: UIView {
     set { playerLayer.player = newValue }
   }
 
+  var stickersCIImage: CIImage? {
+    guard !stickers.isEmpty, let cgImage = stickerContainers.asImage().cgImage else { return nil }
+    return CIImage(cgImage: cgImage)
+  }
+
   func addSticker(_ sticker: UIImage? = nil) {
     let videoRect = playerLayer.videoRect
+
     // Random sticker position in video frame
-    let size: CGFloat = 100
     let randomX = CGFloat.random(in: (videoRect.maxX * 1/5)...(videoRect.maxX * 4/5))
     let randomY = CGFloat.random(in: (videoRect.maxY * 1/5)...(videoRect.maxY * 4/5))
-    let imageView = UIImageView(frame: CGRect(x: randomX, y: randomY, width: size, height: size))
-    if let image = sticker {
-      imageView.image = image
-    } else {
-      imageView.image = UIImage(named: "sticker-\(Int.random(in: 1...2))")
-    }
+
+    let image = sticker ?? UIImage(named: "sticker-\(Int.random(in: 1...2))")!
+    let imageSize = image.size
+    let scaleFactor = min(videoRect.height / image.size.height, image.size.height / videoRect.height)
+    let scaledImageSize = imageSize.applying(.init(scaleX: scaleFactor, y: scaleFactor))
+
+    let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: randomX, y: randomY), size: scaledImageSize))
+    imageView.image = image
 
     imageView.isUserInteractionEnabled = true
 
@@ -69,6 +71,7 @@ class PlayerView: UIView {
   }
 }
 
+// MARK: Gesture
 private extension PlayerView {
   @objc func handlePan(_ sender: UIPanGestureRecognizer) {
     guard
